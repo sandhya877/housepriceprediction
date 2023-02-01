86b11c3049e25dbd35fa45db5a4bfae669e2d859
sandhya877 <80236951+sandhya877@users.noreply.github.com>
2023-02-01 13:54:34 +0530

    added pipeline

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -33,6 +33,11 @@ class Pipeline(Thread):
     experiment_file_path = None
 
     def __init__(self, config: Configuration) -> None:
+      
+      
+      
+      
+      
         try:
             os.makedirs(config.training_pipeline_config.artifact_dir, exist_ok=True)
             Pipeline.experiment_file_path=os.path.join(config.training_pipeline_config.artifact_dir,EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME)
